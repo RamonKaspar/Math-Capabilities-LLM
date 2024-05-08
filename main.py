20fa6a68b5c5361beffc2f83720a55dcd944d39a
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-05-08 19:23:03 +0200

    remove main, not needed

--- main.py
+++
@@ -1,9 +0,0 @@
-from techniques.Baseline import Baseline
-from techniques.PaL import PaL
-
-method = PaL(name="Baseline", dataset="arithmetic", service="openai", model="gpt-3.5-turbo", temperature=0.1, max_token=200)
-
-res = method.query_with_detailed_response("What is 3+3?")
-print(res)
-
-
