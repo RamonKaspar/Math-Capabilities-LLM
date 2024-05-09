43738272d0574a2acc946a71dc7134f2eb06022b
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-05-09 16:49:15 +0200

    Make sure python function for zero-shot scenario is working as well

--- techniques/util.py
+++ techniques/util.py
@@ -1,13 +1,19 @@
 from typing import List
 from openai.types.chat import ChatCompletionMessageParam
 
+import re
+import func_timeout
+import sys
+import io
 
-def create_prompt_gpt35(system_prompt: str, introduction: str, question_prelude: str, question: str, few_shot_examples: List[str], few_shot_answers: List[str]) -> ChatCompletionMessageParam:
+
+def create_prompt_gpt35(few_shot_prompting: bool, system_prompt: str, introduction: str, question_prelude: str, question: str, few_shot_examples: List[str], few_shot_answers: List[str]) -> ChatCompletionMessageParam:
     """
     This function creates the prompt for the GPT-3.5 and GPT-4 model. It adds the system prompt, 
     the introduction, the question prelude, the question and the few shot examples and answers to the conversation.
 
     Args:
+        few_shot_prompting (bool): If True, the few shot examples and answers are added to the conversation.
         system_prompt (str): The system prompt that is added once at the beginning of the conversation
         introduction (str): The introduction, i.e. how to solve it
         question_prelude (str): Introduces the question, e.g. "Question: " or "Answer the following question in Python: "
@@ -19,42 +25,50 @@ def create_prompt_gpt35(system_prompt: str, introduction: str, question_prelude:
         ChatCompletionMessageParam: Prompt for the GPT-3.5 and GPT-4 model
         NOTE: For other models, you (may) have to build the prompts differently
     """
-    assert len(few_shot_examples) == len(few_shot_answers), "Few shot examples and answers should be of the same length"
-    messages = []
-    # Add system prompt
-    messages.append({"role": "system", "content": system_prompt})
-    # Add introduction and first example as user prompt
-    messages.append({"role": "user", "content": introduction + "\n" + "Here is one example how to do it:" + "\n\n" + question_prelude + few_shot_examples[0] + "\n\n" + few_shot_answers[0] + "\n\n" + "Now it's your turn."})
-    # Add remaining four examples and answers to the conversation
-    for i in range(1, len(few_shot_examples)):        
-        messages.append({"role": "user", "content": question_prelude + " " + few_shot_examples[i]})
-        messages.append({"role": "assistant", "content": few_shot_answers[i]})
-    # Prepare the final question that has to be answeted
-    messages.append({"role": "user", "content": question_prelude + question})
+    if few_shot_prompting:
+        assert len(few_shot_examples) == len(few_shot_answers), "Few shot examples and answers should be of the same length"
+        messages = []
+        # Add system prompt
+        messages.append({"role": "system", "content": system_prompt})
+        # Add introduction and first example as user prompt
+        messages.append({"role": "user", "content": introduction + "\n" + "Here is one example how to do it:" + "\n\n" + question_prelude + few_shot_examples[0] + "\n\n" + few_shot_answers[0] + "\n\n" + "Now it's your turn."})
+        # Add remaining four examples and answers to the conversation
+        for i in range(1, len(few_shot_examples)):        
+            messages.append({"role": "user", "content": question_prelude + " " + few_shot_examples[i]})
+            messages.append({"role": "assistant", "content": few_shot_answers[i]})
+        # Prepare the final question that has to be answeted
+        messages.append({"role": "user", "content": question_prelude + question})
+    else:
+        messages = []
+        # Add system prompt
+        messages.append({"role": "system", "content": system_prompt})
+        # Add introduction as user prompt and Prepare the final question that has to be answeted
+        messages.append({"role": "user", "content": introduction + "\n" + question_prelude + question})
     return messages
-    
 
 
-import func_timeout
 
 # Code is taken from here: https://github.com/XuZhao0/Model-Selection-Reasoning/blob/main/src/tool.py
+# However, quite is modified to fit the needs of this project
 def execute_solution_function(code_string: str):
     """
-    Executes Python code that is expected to define and return the result of a function named 'solution'.
+    Executes Python code that may define and return the result of a function named 'solution'
+    or execute standalone code if 'solution' function is not present.
+    If there is no function called solution(), this method assumes the result is printed to the console.
     The function compiles the code in a restricted environment and attempts to call 'solution()'.
     
     Args:
         code (str): A string of Python code which should contain a function definition called 'solution'.
     
     Returns:
-        The return value of the 'solution()' function if successfully called. Returns None if the function
-        does not exist, if an error occurs during its execution, or if the code does not adhere to safety constraints.
+        The return value of the 'solution()' function if it exists, or the result of the evaluated code.
+        Returns None if the function does not exist, if an error occurs during its execution,
+        or if the code does not adhere to safety constraints.
     """
     def execute(x, code_return):
         try:
             exec(x)
             locals_ = locals()
-            # 
             solution = locals_.get('solution', None)
             if solution is not None:
                 return solution()
@@ -72,27 +86,47 @@ def execute_solution_function(code_string: str):
 
     # === find code snippets between def solution(): and return ===
     try:
-        code_list = code_string.strip().split('\n')
+        if 'def solution():' in code_string:
+            code_list = code_string.strip().split('\n')
 
-        new_code_list = []
-        all_codes = []
-        code_return = 'ans'
+            new_code_list = []
+            all_codes = []
+            code_return = 'ans'
 
-        for i in range(len(code_list)):
-            if code_list[i].strip() == 'def solution():':
-                new_code_list.append(code_list[i])
-                for j in range(i+1, len(code_list)):
-                    if code_list[j].startswith('    '):
-                        new_code_list.append(code_list[j])
-                    if 'return ' in code_list[j]:
-                        code_return = code_list[j].split('return ')[1].strip()
-                all_codes.append('\n'.join(new_code_list))
-                new_code_list = []
-        new_code = all_codes[-1]
-        ans = func_timeout.func_timeout(
-            3, execute, args=(new_code, code_return,))
-        ans = ans if ans is not None else ans
-    except func_timeout.FunctionTimedOut:
+            for i in range(len(code_list)):
+                if code_list[i].strip() == 'def solution():':
+                    new_code_list.append(code_list[i])
+                    for j in range(i+1, len(code_list)):
+                        if code_list[j].startswith('    '):
+                            new_code_list.append(code_list[j])
+                        if 'return ' in code_list[j]:
+                            code_return = code_list[j].split('return ')[1].strip()
+                    all_codes.append('\n'.join(new_code_list))
+                    new_code_list = []
+            new_code = all_codes[-1]
+            ans = func_timeout.func_timeout(
+                3, execute, args=(new_code, code_return,))
+            ans = ans if ans is not None else ans
+        else:
+            # Extract code blocks from string using both markdown triple quotes with 'python' and without.
+            triple_quote_blocks = re.findall(r"```(?:python)?(.*?)```", code_string, flags=re.DOTALL)
+            if not triple_quote_blocks:
+                triple_quote_blocks = re.findall(r"'''(?:python)?(.*?)'''", code_string, flags=re.DOTALL)
+            # Concatenate all extracted blocks for execution.
+            code_to_execute = '\n'.join(block.strip() for block in triple_quote_blocks) if triple_quote_blocks else code_string
+            # Capture output printed to the console during execution.
+            old_stdout = sys.stdout
+            redirected_output = sys.stdout = io.StringIO()
+            try:
+                # Executing the concatenated code blocks
+                exec(code_to_execute, globals(), locals())
+            finally:  # Ensure that stdout is reset back to original
+                sys.stdout = old_stdout
+            printed_output = redirected_output.getvalue()
+            # Use regex to find all numbers, capture the last one if exists
+            numbers = re.findall(r"[-+]?\d*\.\d+|\d+", printed_output)
+            ans = numbers[-1]
+    except Exception as e:
         ans = None
     try:
         ans = float(ans) if ans is not None else ans
@@ -100,8 +134,6 @@ def execute_solution_function(code_string: str):
         ans = None
     return ans
 
-
-import re 
 
 def extract_number(answer_str: str, answer_prefix: str = None) -> float:
     """
