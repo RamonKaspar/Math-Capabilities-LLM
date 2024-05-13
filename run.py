4d11b5d7655e07e2a863d11c0f2483995d63181b
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-05-13 11:18:19 +0200

    Script for running evaluations

--- run.py
+++ run.py
@@ -5,23 +5,37 @@ from tqdm import tqdm
 
 from techniques.Baseline import Baseline
 from techniques.PaL import PaL
+from techniques.CoT import CoT
+from techniques.RolePlaying import RolePlaying
+from techniques.DeclarativeSymPy import DeclarativeSymPy
+from techniques.ModelSelection import ModelSelection
 
-def technique_factory(technique_name, dataset, service, model, temperature, max_token):
+def technique_factory(technique_name, few_shot_prompting, dataset, service, model, temperature, max_token):
     """ Factory function to create an instance of a technique based on the input parameters.
     NOTE: If you add a new technique, you should add a new condition to this function.
     """
     if technique_name == "Baseline":
-        return Baseline(name=technique_name, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
+        return Baseline(name=technique_name, few_shot_prompting=few_shot_prompting, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
     elif technique_name == "PaL":
-        return PaL(name=technique_name, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
+        return PaL(name=technique_name, few_shot_prompting=few_shot_prompting, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
+    elif technique_name == "CoT":
+        return CoT(name=technique_name, few_shot_prompting=few_shot_prompting, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
+    elif technique_name == "RolePlaying":
+        # if few_shot_prompting:
+        #     raise ValueError("RolePlaying technique does not support few-shot prompting.")
+        return RolePlaying(name=technique_name, few_shot_prompting=few_shot_prompting, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
+    elif technique_name == "DeclarativeSymPy":
+        return DeclarativeSymPy(name=technique_name, few_shot_prompting=few_shot_prompting, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
+    elif technique_name == "ModelSelection":
+        return ModelSelection(name=technique_name, few_shot_prompting=few_shot_prompting, dataset=dataset, service=service, model=model, temperature=temperature, max_token=max_token)
     else:
         raise ValueError("Unsupported technique name")
 
 
-def run_evaluation(technique_name, dataset, service, model, temperature, max_token):
+def run_evaluation(technique_name, few_shot_prompting, dataset, service, model, temperature, max_token):
     """Executes the evaluation of a specified technique on a given dataset and saves the results as a CSV file."""
     assert(dataset in ["arithmetic_100", "wordProblems_100", "geometry_100", "arithmetic_1000", "wordProblems_1000", "geometry_1000"])
-    technique = technique_factory(technique_name, dataset.split('_')[0], service, model, temperature, max_token)
+    technique = technique_factory(technique_name, few_shot_prompting, dataset.split('_')[0], service, model, temperature, max_token)
     
     dataset_df = pd.read_csv(f"datasets/{dataset}.csv")
     results = []
@@ -48,35 +62,25 @@ def run_evaluation(technique_name, dataset, service, model, temperature, max_tok
         response['latency_in_seconds'] = latency
         results.append(response)
         
-        # Sleep for 1 second to avoid rate limiting
-        sleep(1)
+        # Sleep for 2 second to avoid rate limits
+        sleep(2)
         
     results_df = pd.DataFrame(results)
-    results_df.to_csv(f"evaluation/data/{technique_name}_{dataset}_{service}_{model}.csv", index=False)
-    print(f"Results saved to evaluation/data/{technique_name}_{dataset}_{service}_{model}.csv")
-    
+    few_shot_or_zero_shot = "Few-shot" if few_shot_prompting else "Zero-shot"
+    results_df.to_csv(f"evaluation/data/{technique_name}_{few_shot_or_zero_shot}_{dataset}_{service}_{model}.csv", index=False)
+    print(f"Results saved to evaluation/data/{technique_name}_{few_shot_or_zero_shot}_{dataset}_{service}_{model}.csv")
+
+
 if __name__ == "__main__":
     # Fix the testing parameters
-    DATASET = "wordProblems_100"
     SERVICE = "azure"
     MODEL = "gpt-35-turbo"
     TEMPERATURE = 0
-    MAX_TOKEN = 200
+    MAX_TOKEN = 400
     
-    # for technique_name in ["Baseline", "PaL"]:
-    #     print(f"Running the evaluation for the {technique_name} technique on the {DATASET} dataset using the {MODEL} model.")
-    #     run_evaluation(technique_name, DATASET, SERVICE, MODEL, TEMPERATURE, MAX_TOKEN)
-    #     print("Finished.")
-    
-    # Evaluate Baseline
-    print(f"Evaluating the Baseline technique on the {DATASET} dataset using the GPT-3.5-turbo model.")
-    run_evaluation("Baseline", DATASET, SERVICE, MODEL, TEMPERATURE, MAX_TOKEN)
-    print("Finished.")
-    
-    # Evaluate PaL
-    print(f"Evaluating the PaL technique on the {DATASET} dataset using the GPT-3.5-turbo model.")
-    run_evaluation("PaL", DATASET, SERVICE, MODEL, TEMPERATURE, MAX_TOKEN)
-    print("Finished.")
-
-
-
+    for technique_name in ["Baseline", "PaL", "CoT", "RolePlaying", "DeclarativeSymPy", "ModelSelection"]:
+        for dataset in ["arithmetic_100", "wordProblems_100", "geometry_100"]:
+            for few_shot_prompting in [True, False]:
+                print(f"Running the evaluation for the {technique_name} {'Few-shot' if few_shot_prompting else 'Zero-shot'} technique on the {dataset} dataset using the {MODEL} model.")
+                run_evaluation(technique_name, few_shot_prompting, dataset, SERVICE, MODEL, TEMPERATURE, MAX_TOKEN)
+                print("Finished.")
